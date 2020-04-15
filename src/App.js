2059b3444cf20a0860bdcb2d5b0fca4c9c00bad3
Nuel822 <Clinton.bob72@yahoo.com>
2020-04-15 13:11:24 +0100

    updated app with shop-page, collectio-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 import { Homepage } from "./Pages/Homepage/homepage.component";
 import './Pages/Homepage/homepage.style.scss';
 import { Switch, Route } from "react-router-dom";
-
+import ShopPage  from './Pages/Shop/shoppage.component';
 
 
 function App() {
 
-const HatsPage = () => {
-  return <h1>HatsPage</h1>
-}
+
   return (
     <Switch>
       <Route exact path="/" component={Homepage}/>
-      <Route path="/hats" component={HatsPage}/>
+      <Route path="/shop" component={ShopPage}/>
     </Switch>
 
   );
